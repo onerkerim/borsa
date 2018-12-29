b4bd84fdb82db5056868ee2285487215097aef5c
onerkerim <onerkerim@hotmail.com>
2018-12-29 16:08:34 +0200

    first5

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<script src="http://localhost:<?php echo $socketIoPort; ?>/socket.io/socket.io.js"></script>
+<script src="http://weborsa.herokuapp.com:<?php echo $socketIoPort; ?>/socket.io/socket.io.js"></script>
 <script src="assets/js/jquery-3.3.1.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 
