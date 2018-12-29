d2a0534f1243f114c6d7d72ef19ce1c190a7b333
onerkerim <onerkerim@hotmail.com>
2018-12-29 16:42:05 +0200

    First node app deploy

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
 <script src="http://weborsa.herokuapp.com:<?php echo $socketIoPort; ?>/socket.io/socket.io.js"></script>
 <script src="assets/js/jquery-3.3.1.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-
-
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js?v=1"></script>
 <?php if(isset($ajaxContent)) 
 { 
 	if(is_array($ajaxContent))
